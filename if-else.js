96d1f39dd1358f91d1d66bda0feb98e1aeef18cd
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-03-16 14:12:25 -0400

    3 belajar if else

---
+++ if-else.js
@@ -0,0 +1,14 @@
+function jalanSatu() {
+    var nilai = 2;
+
+    if (nilai === 1) {
+        console.log('satu');
+    } else if (nilai == 2) {
+        console.log('dua');
+    }else{
+        console.log('salah');
+        
+    }
+}
+
+jalanSatu();
