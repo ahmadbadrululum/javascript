6bd7fbc759413faab2506fb6df876d8686271715
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-03-16 14:12:48 -0400

    4 belajar switch

---
+++ switch.js
@@ -0,0 +1,20 @@
+function switchCase() {
+    var nilai = 6;
+    switch (nilai) {
+        case 1:
+            console.log('satu');
+            break;
+        case 2:
+            console.log('dua');
+            break;
+        case 3:
+            console.log('tiga');
+            break;
+        default:
+            console.log('empat');
+            break;
+    }
+}
+
+
+switchCase();
