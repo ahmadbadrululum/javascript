06ca83662ee17c69c03542926bdcf98edf7459d1
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-03-16 14:11:34 -0400

    menenal variabel

---
+++ index.js
@@ -0,0 +1,14 @@
+var msg1 = 'hallo codding1';
+let msg2 = 'hallo codding2';
+const msg3 = 'hallo codding3';
+
+if (true) {
+    var msg1 = 'halo gays';
+    let msg3 = 'halo gays3';
+}
+
+
+
+console.log(msg1);
+console.log(msg2);
+console.log(msg3);
