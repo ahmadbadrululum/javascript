1de83feb9300af1147600b820fd9353cc920884c
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-03-16 14:11:54 -0400

    function

---
+++ function.js
@@ -0,0 +1,18 @@
+
+//  js old 
+function jalanSatu() {
+    console.log('test1');
+}
+const jalanDua = function() {
+    console.log('test2');
+}
+
+jalanSatu();
+jalanDua();
+
+// jsx
+const jalanTiga = ()=>{
+    console.log('test3');
+}
+
+jalanTiga();
