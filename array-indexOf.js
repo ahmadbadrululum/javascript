5cdd41cbc882e276057c01334b36f6ccdfe761df
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-04-09 00:39:37 +0700

    mencari nilai pada array

---
+++ array-indexOf.js
@@ -0,0 +1,14 @@
+function indexofArray() {
+    kota = ['demak','semarang','kendal','jakarta',]
+    console.log(kota);
+    
+    // FUNGSI INDEXOF JS UNTUK MENCARI INDEX PADA ARRAY;
+    // return kota.indexOf('demak');
+    return kota.indexOf('kendal');
+    // mencai dan print pada urutan index
+    
+}
+
+
+
+console.log(indexofArray());
