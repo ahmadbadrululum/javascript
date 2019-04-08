6e81a4dd30533a85b9a43e0426e96b7be5170edd
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-04-09 00:32:56 +0700

    menambahkan nilai dia awal index

---
+++ array-unshift.js
@@ -0,0 +1,12 @@
+function unshiftArray() {
+    kota = ['demak','semarang','kendal','jakarta',]
+    console.log(kota);
+    
+
+    // FUNGSI UNSSHIFT JS UNTUK MENAMBAH INDEX PERTAMA PADA ARRAY YAITU INDEX 0;
+    kota.unshift('PAMULANG', 'MADURA');
+    return kota;
+}
+
+
+console.log(unshiftArray());
