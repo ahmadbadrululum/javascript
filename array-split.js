f53fd917ebf37bb8a7fa449528f6be4b8b09f1bb
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-04-09 00:50:37 +0700

    splite mengganti string menjadi array

---
+++ array-split.js
@@ -0,0 +1,12 @@
+function splitArray() {
+    var kota = "demak,bekasi,jakarta" 
+    var arrKota = kota.split(",")
+
+    // FUNGSI SPLIT MENGUBAH STRING MENJADI ARRAY;
+    return arrKota;
+    
+}
+
+
+
+console.log(splitArray());
