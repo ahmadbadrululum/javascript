987766a61f369cced4e36803bf1a076176edab37
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-04-09 03:04:37 +0700

    contoh tugas parameter dan filter

--- functionParameter.js
+++ functionParameter.js
@@ -1,29 +1,29 @@
 
 // string
-function panggilParameters($string) {
-    console.log($string);
+function panggilParameters(string) {
+    console.log(string);
 }
 
 panggilParameters('string')
 
 // NUMBER
-function panggilParameters2($number) {
-    console.log($number);
+function panggilParameters2(number) {
+    console.log(number);
 }
 panggilParameters2(123123)
 
 
 // / NUMBER dan string
-function panggilParameters3($number, $string) {
-    console.log($number, $string);
+function panggilParameters3(number, string) {
+    console.log(number, string);
 }
 panggilParameters3(123123, 'ahmad string');
 
 
 // / NUMBER dan string dan array
-function panggilParameters4($number, $string, $array) {
-    console.log($number, $array);
-    console.log($string);
+function panggilParameters4(number, string, array) {
+    console.log(number, array);
+    console.log(string);
 }
 panggilParameters4(123123, 'ahmad string', ['ahmad', 'badrul', 'ulum']);
 
