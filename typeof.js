8a655e9b5e3dbeb77ad338b03d8d11fa589c2146
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-04-08 23:46:03 +0700

    mengecek type data

---
+++ typeof.js
@@ -0,0 +1,16 @@
+function checkTypeData() {
+    var data = "nama";
+    var data1 = 123123123;
+    var data2 = [1,2,4];
+    var data3 = {
+        nama: "ahmad",
+        umur : 10
+    }
+    console.log(typeof(data));
+    console.log(typeof(data1));
+    console.log(typeof(data2));
+    console.log(typeof(data3));
+}
+
+
+checkTypeData();
