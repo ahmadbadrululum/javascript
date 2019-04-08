0cf8c6ded283abd75ebf66b60980d4866f96dec0
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-04-09 00:43:23 +0700

    concat menggabungkan 2 array/ merge array

---
+++ array-concat.js
@@ -0,0 +1,15 @@
+function concatArray() {
+    var kota = ['demak','semarang','kendal','jakarta',]
+    var kabupaten = ['solo','jogja']
+
+    
+    // FUNGSI CONCAT JS UNTUK MERGE ARRAY ATAU MENGGABUNGKAN ARRAY;
+
+    var gabung = kota.concat(kabupaten);
+    return gabung;
+    
+}
+
+
+
+console.log(concatArray());
