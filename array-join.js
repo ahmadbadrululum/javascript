3c7bcab86548819abb602ebb3a0ecc928f2f1657
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-04-09 00:53:33 +0700

    join mengubah array menjadi string

---
+++ array-join.js
@@ -0,0 +1,12 @@
+function joinArray() {
+    var kota = ["demak","bekasi","jakarta"]
+    var arrKota = kota.join(",")
+
+    // FUNGSI SPLIT MENGUBAH STRING MENJADI ARRAY;
+    return arrKota;
+    
+}
+
+
+
+console.log(joinArray());
