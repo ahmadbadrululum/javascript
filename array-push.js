e675a373a4f34ea2ad62c5056742860f710ecfb9
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-04-09 00:20:33 +0700

    push menambahkan nilai array dipaling belakang

---
+++ array-push.js
@@ -0,0 +1,15 @@
+// MANIPULASI ARRAY PUSH
+
+
+function pushArray() {
+    kota = ['demak','semarang','kendal','jakarta']
+    console.log(kota);
+    
+    // FUNGSI PUSH JS UNTUK MENAMBAHKAN INDEX PALING BELAKANG;
+    var kota2 = kota.push('nama')
+    // console.log(kota2);
+    return kota;
+}
+
+
+console.log(pushArray());
