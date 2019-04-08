2b8ecb7ec71e82e80b47daa1b92814e36c18485d
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-04-09 01:04:03 +0700

    modulus mencari hasil sisa bagi  dengan powerfull

---
+++ modulus.js
@@ -0,0 +1,17 @@
+// Operator Modulus
+// Operator Modulus adalah sebuah operasi yang menghasilkan sisa pembagian dari suatu bilangan (terhadap bilangan pembaginya). Di javascript, modulus dilambangkan dengan simbol persen (%)
+
+// DIPERGUNAKAN MENCARI NILAI GANJIL DAN GENAP
+
+// MENCARI NILAI HASIL BAGI DENGAN POWERFULL MODULUS 
+function panggilModulus() {
+    // var nilai1  = 120;
+    // var nilai2  = 32;
+    var nilai1  = 10;
+    var nilai2  = 3;
+
+
+    var hasil  = nilai1 % nilai2 
+    return hasil;
+}
+console.log(panggilModulus());
